42c8992491991c0d4b1a1764ce8f2d6c120730f0
Diana Corbacho <diana.corbacho@erlang-solutions.com>
2013-06-04 17:14:59 +0100

    Remove unused code

--- test/webserver_utils.erl
+++ test/webserver_utils.erl
@@ -7,34 +7,11 @@
 default_string() ->
     ?DEFAULT_STRING.
 
-%%% Responders
-simple_response(Module, Socket, _Request, _Headers, Body) ->
-    Module:send(
-        Socket,
-        [
-            "HTTP/1.1 200 OK\r\n"
-            "Content-type: text/plain\r\nContent-length: 14\r\n"
-            "X-Test-Orig-Body: ", Body, "\r\n\r\n"
-            ?DEFAULT_STRING
-        ]
-    ).
-
 empty_body(Module, Socket, _, _, _) ->
     Module:send(
         Socket,
         "HTTP/1.1 200 OK\r\n"
         "Content-type: text/plain\r\nContent-length: 0\r\n\r\n"
-    ).
-
-copy_body(Module, Socket, _, _, Body) ->
-    Module:send(
-        Socket,
-        [
-            "HTTP/1.1 200 OK\r\n"
-            "Content-type: text/plain\r\nContent-length: "
-            ++ integer_to_list(size(Body)) ++ "\r\n\r\n",
-            Body
-        ]
     ).
 
 copy_body_100_continue(Module, Socket, _, _, Body) ->
@@ -48,33 +25,6 @@ copy_body_100_continue(Module, Socket, _, _, Body) ->
             Body
         ]
     ).
-
-respond_and_close(Module, Socket, _, _, Body) ->
-    Pid = list_to_pid(binary_to_list(Body)),
-    Module:send(
-        Socket,
-        "HTTP/1.1 200 OK\r\n"
-        "Connection: close\r\n"
-        "Content-type: text/plain\r\nContent-length: 14\r\n\r\n"
-        ?DEFAULT_STRING
-    ),
-    {error, closed} = Module:recv(Socket, 0),
-    Pid ! closed,
-    Module:close(Socket).
-
-respond_and_wait(Module, Socket, _, _, Body) ->
-    Pid = list_to_pid(binary_to_list(Body)),
-    Module:send(
-        Socket,
-        "HTTP/1.1 200 OK\r\n"
-        "Content-type: text/plain\r\nContent-length: 14\r\n\r\n"
-        ?DEFAULT_STRING
-    ),
-    % We didn't signal a connection close, but we want the client to do that
-    % any way
-    {error, closed} = Module:recv(Socket, 0),
-    Pid ! closed,
-    Module:close(Socket).
 
 pre_1_1_server(Module, Socket, _, _, Body) ->
     Pid = list_to_pid(binary_to_list(Body)),
@@ -124,47 +74,6 @@ no_content_length_1_0(Module, Socket, _, _, _) ->
         "Content-type: text/plain\r\n\r\n"
         ?DEFAULT_STRING
     ).
-
-basic_auth_responder(User, Passwd) ->
-    fun(Module, Socket, _Request, Headers, _Body) ->
-        case proplists:get_value("Authorization", Headers) of
-            undefined ->
-                Module:send(
-                    Socket,
-                    [
-                        "HTTP/1.1 401 Unauthorized\r\n",
-                        "Content-Type: text/plain\r\n",
-                        "Content-Length: 12\r\n\r\n",
-                        "missing_auth"
-                    ]
-                );
-            "Basic " ++ Auth ->
-                [U, P] = string:tokens(
-                    binary_to_list(base64:decode(iolist_to_binary(Auth))), ":"),
-                case {U, P} of
-                    {User, Passwd} ->
-                        Module:send(
-                            Socket,
-                            [
-                                "HTTP/1.1 200 OK\r\n",
-                                "Content-Type: text/plain\r\n",
-                                "Content-Length: 2\r\n\r\n",
-                                "OK"
-                            ]
-                        );
-                    _ ->
-                        Module:send(
-                            Socket,
-                            [
-                                "HTTP/1.1 401 Unauthorized\r\n",
-                                "Content-Type: text/plain\r\n",
-                                "Content-Length: 10\r\n\r\n",
-                                "wrong_auth"
-                            ]
-                        )
-                end
-        end
-    end.
 
 trailing_space_header(Module, Socket, _, _, _) ->
     Module:send(
